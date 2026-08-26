8ee25eb47757360987dba24b4018e7ef281e4af5
hussenwww1010-sys <hussenwww1010@gmail.com>
2026-08-26 23:27:12 +0300

    Update channels.js

--- channels.js
+++ channels.js
@@ -1,57 +1,49 @@
 const channels = [
 
-    // ==============================
+    // =========================================================
     // beIN SPORTS - SOURCE 1
-    // ==============================
+    // =========================================================
 
     {
         name: "beIN SPORT 1HD",
         package: "beIN SPORTS",
         url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=1330437&extension=ts"
     },
-
     {
         name: "beIN SPORT 2HD",
         package: "beIN SPORTS",
         url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=1330438&extension=ts"
     },
-
     {
         name: "beIN SPORT 3HD",
         package: "beIN SPORTS",
         url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=1411381&extension=ts"
     },
-
     {
         name: "beIN SPORT 4HD",
         package: "beIN SPORTS",
         url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=1411380&extension=ts"
     },
-
     {
         name: "beIN SPORT 5HD",
         package: "beIN SPORTS",
         url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=1411379&extension=ts"
     },
-
     {
         name: "beIN SPORT 6HD",
         package: "beIN SPORTS",
         url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=1411378&extension=ts"
     },
-
     {
         name: "beIN SPORT 7HD",
         package: "beIN SPORTS",
         url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=1411377&extension=ts"
     },
-
     {
         name: "beIN SPORT 8HD",
         package: "beIN SPORTS",
         url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=1411376&extension=ts"
     },
-
     {
         name: "beIN SPORT 9HD",
         package: "beIN SPORTS",
@@ -59,58 +51,50 @@ const channels = [
     },
 
 
-    // ==============================
+    // =========================================================
     // beIN SPORTS - SOURCE 2
-    // ==============================
+    // =========================================================
 
     {
         name: "beIN SPORT 1HD - 2",
         package: "beIN SPORTS",
         url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=1660413&extension=ts"
     },
-
     {
         name: "beIN SPORT 2HD - 2",
         package: "beIN SPORTS",
         url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=1660411&extension=ts"
     },
-
     {
         name: "beIN SPORT 3HD - 2",
         package: "beIN SPORTS",
         url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=1660409&extension=ts"
     },
-
     {
         name: "beIN SPORT 4HD - 2",
         package: "beIN SPORTS",
         url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=1660407&extension=ts"
     },
-
     {
         name: "beIN SPORT 5HD - 2",
         package: "beIN SPORTS",
         url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=1660405&extension=ts"
     },
-
     {
         name: "beIN SPORT 6HD - 2",
         package: "beIN SPORTS",
         url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=1660403&extension=ts"
     },
-
     {
         name: "beIN SPORT 7HD - 2",
         package: "beIN SPORTS",
         url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=1660401&extension=ts"
     },
-
     {
         name: "beIN SPORT 8HD - 2",
         package: "beIN SPORTS",
         url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=1660399&extension=ts"
     },
-
     {
         name: "beIN SPORT 9HD - 2",
         package: "beIN SPORTS",
@@ -118,63 +102,412 @@ const channels = [
     },
 
 
-    // ==============================
+    // =========================================================
     // ALWAN SPORT
-    // ==============================
+    // =========================================================
 
     {
-        name: "ALWAN SPORT 1HD",
+        name: "ALWAN SPORT 1 - 4K",
         package: "ALWAN SPORT",
-        url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=1859098&extension=ts"
+        logo: "http://vodkom.com/LOGO/TV_ALWAN/alwan1-4k.png",
+        quality: "4K",
+        channel: 1,
+        url: "http://sansat.cc:88/02022506639475/7317926030/852917"
+    },
+    {
+        name: "ALWAN SPORT 1 - HD",
+        package: "ALWAN SPORT",
+        logo: "http://vodkom.com/LOGO/TV_ALWAN/alwan1-hd.png",
+        quality: "HD",
+        channel: 1,
+        url: "http://sansat.cc:88/02022506639475/7317926030/852915"
+    },
+    {
+        name: "ALWAN SPORT 1 - SD",
+        package: "ALWAN SPORT",
+        logo: "http://vodkom.com/LOGO/TV_ALWAN/alwan1-sd.png",
+        quality: "SD",
+        channel: 1,
+        url: "http://sansat.cc:88/02022506639475/7317926030/852916"
     },
 
     {
-        name: "ALWAN SPORT 2HD",
+        name: "ALWAN SPORT 2 - 4K",
         package: "ALWAN SPORT",
-        url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=1859097&extension=ts"
+        logo: "http://vodkom.com/LOGO/TV_ALWAN/alwan2-4k.png",
+        quality: "4K",
+        channel: 2,
+        url: "http://sansat.cc:88/02022506639475/7317926030/852920"
+    },
+    {
+        name: "ALWAN SPORT 2 - HD",
+        package: "ALWAN SPORT",
+        logo: "http://vodkom.com/LOGO/TV_ALWAN/alwan2-hd.png",
+        quality: "HD",
+        channel: 2,
+        url: "http://sansat.cc:88/02022506639475/7317926030/852918"
+    },
+    {
+        name: "ALWAN SPORT 2 - SD",
+        package: "ALWAN SPORT",
+        logo: "http://vodkom.com/LOGO/TV_ALWAN/alwan2-sd.png",
+        quality: "SD",
+        channel: 2,
+        url: "http://sansat.cc:88/02022506639475/7317926030/852919"
     },
 
     {
-        name: "ALWAN SPORT 3HD",
+        name: "ALWAN SPORT 3 - 4K",
         package: "ALWAN SPORT",
-        url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=1859096&extension=ts"
+        logo: "http://vodkom.com/LOGO/TV_ALWAN/alwan3-4k.png",
+        quality: "4K",
+        channel: 3,
+        url: "http://sansat.cc:88/02022506639475/7317926030/852923"
+    },
+    {
+        name: "ALWAN SPORT 3 - HD",
+        package: "ALWAN SPORT",
+        logo: "http://vodkom.com/LOGO/TV_ALWAN/alwan3-hd.png",
+        quality: "HD",
+        channel: 3,
+        url: "http://sansat.cc:88/02022506639475/7317926030/852921"
+    },
+    {
+        name: "ALWAN SPORT 3 - SD",
+        package: "ALWAN SPORT",
+        logo: "http://vodkom.com/LOGO/TV_ALWAN/alwan3-sd.png",
+        quality: "SD",
+        channel: 3,
+        url: "http://sansat.cc:88/02022506639475/7317926030/852922"
     },
 
     {
-        name: "ALWAN SPORT 4HD",
+        name: "ALWAN SPORT 4 - 4K",
         package: "ALWAN SPORT",
-        url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=1859095&extension=ts"
+        logo: "http://vodkom.com/LOGO/TV_ALWAN/alwan4-4k.png",
+        quality: "4K",
+        channel: 4,
+        url: "http://sansat.cc:88/02022506639475/7317926030/852926"
+    },
+    {
+        name: "ALWAN SPORT 4 - HD",
+        package: "ALWAN SPORT",
+        logo: "http://vodkom.com/LOGO/TV_ALWAN/alwan4-hd.png",
+        quality: "HD",
+        channel: 4,
+        url: "http://sansat.cc:88/02022506639475/7317926030/852924"
+    },
+    {
+        name: "ALWAN SPORT 4 - SD",
+        package: "ALWAN SPORT",
+        logo: "http://vodkom.com/LOGO/TV_ALWAN/alwan4-sd.png",
+        quality: "SD",
+        channel: 4,
+        url: "http://sansat.cc:88/02022506639475/7317926030/852925"
     },
 
     {
-        name: "ALWAN SPORT 5HD",
+        name: "ALWAN SPORT 5 - 4K",
         package: "ALWAN SPORT",
-        url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=1859094&extension=ts"
+        logo: "http://vodkom.com/LOGO/TV_ALWAN/alwan5-4k.png",
+        quality: "4K",
+        channel: 5,
+        url: "http://sansat.cc:88/02022506639475/7317926030/852929"
+    },
+    {
+        name: "ALWAN SPORT 5 - HD",
+        package: "ALWAN SPORT",
+        logo: "http://vodkom.com/LOGO/TV_ALWAN/alwan5-hd.png",
+        quality: "HD",
+        channel: 5,
+        url: "http://sansat.cc:88/02022506639475/7317926030/852927"
+    },
+    {
+        name: "ALWAN SPORT 5 - SD",
+        package: "ALWAN SPORT",
+        logo: "http://vodkom.com/LOGO/TV_ALWAN/alwan5-sd.png",
+        quality: "SD",
+        channel: 5,
+        url: "http://sansat.cc:88/02022506639475/7317926030/852928"
     },
 
     {
-        name: "ALWAN SPORT 6HD",
+        name: "ALWAN SPORT 6 - 4K",
         package: "ALWAN SPORT",
-        url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=1859093&extension=ts"
+        logo: "http://vodkom.com/LOGO/TV_ALWAN/alwan6-4k.png",
+        quality: "4K",
+        channel: 6,
+        url: "http://sansat.cc:88/02022506639475/7317926030/852932"
+    },
+    {
+        name: "ALWAN SPORT 6 - HD",
+        package: "ALWAN SPORT",
+        logo: "http://vodkom.com/LOGO/TV_ALWAN/alwan6-hd.png",
+        quality: "HD",
+        channel: 6,
+        url: "http://sansat.cc:88/02022506639475/7317926030/852930"
+    },
+    {
+        name: "ALWAN SPORT 6 - SD",
+        package: "ALWAN SPORT",
+        logo: "http://vodkom.com/LOGO/TV_ALWAN/alwan6-sd.png",
+        quality: "SD",
+        channel: 6,
+        url: "http://sansat.cc:88/02022506639475/7317926030/852931"
     },
 
 
-    // ==============================
+    // =========================================================
+    // SHAHID SPORTS
+    // =========================================================
+
+    {
+        name: "SHAHID SPORTS 1 - 4K",
+        package: "SHAHID SPORTS",
+        quality: "4K",
+        channel: 1,
+        url: "http://sansat.cc:88/02022506639475/7317926030/1079684"
+    },
+    {
+        name: "SHAHID SPORTS 1 - FHD",
+        package: "SHAHID SPORTS",
+        quality: "FHD",
+        channel: 1,
+        url: "http://sansat.cc:88/02022506639475/7317926030/1079688"
+    },
+    {
+        name: "SHAHID SPORTS 1 - HD",
+        package: "SHAHID SPORTS",
+        quality: "HD",
+        channel: 1,
+        url: "http://sansat.cc:88/02022506639475/7317926030/1079692"
+    },
+    {
+        name: "SHAHID SPORTS 1 - SD",
+        package: "SHAHID SPORTS",
+        quality: "SD",
+        channel: 1,
+        url: "http://sansat.cc:88/02022506639475/7317926030/1079696"
+    },
+    {
+        name: "SHAHID SPORTS 1 - LOW",
+        package: "SHAHID SPORTS",
+        quality: "LOW",
+        channel: 1,
+        url: "http://sansat.cc:88/02022506639475/7317926030/1079700"
+    },
+
+    {
+        name: "SHAHID SPORTS 2 - 4K",
+        package: "SHAHID SPORTS",
+        quality: "4K",
+        channel: 2,
+        url: "http://sansat.cc:88/02022506639475/7317926030/1079685"
+    },
+    {
+        name: "SHAHID SPORTS 2 - FHD",
+        package: "SHAHID SPORTS",
+        quality: "FHD",
+        channel: 2,
+        url: "http://sansat.cc:88/02022506639475/7317926030/1079689"
+    },
+    {
+        name: "SHAHID SPORTS 2 - HD",
+        package: "SHAHID SPORTS",
+        quality: "HD",
+        channel: 2,
+        url: "http://sansat.cc:88/02022506639475/7317926030/1079693"
+    },
+    {
+        name: "SHAHID SPORTS 2 - SD",
+        package: "SHAHID SPORTS",
+        quality: "SD",
+        channel: 2,
+        url: "http://sansat.cc:88/02022506639475/7317926030/1079697"
+    },
+    {
+        name: "SHAHID SPORTS 2 - LOW",
+        package: "SHAHID SPORTS",
+        quality: "LOW",
+        channel: 2,
+        url: "http://sansat.cc:88/02022506639475/7317926030/1079702"
+    },
+
+    {
+        name: "SHAHID SPORTS 3 - 4K",
+        package: "SHAHID SPORTS",
+        quality: "4K",
+        channel: 3,
+        url: "http://sansat.cc:88/02022506639475/7317926030/1079686"
+    },
+    {
+        name: "SHAHID SPORTS 3 - FHD",
+        package: "SHAHID SPORTS",
+        quality: "FHD",
+        channel: 3,
+        url: "http://sansat.cc:88/02022506639475/7317926030/1079690"
+    },
+    {
+        name: "SHAHID SPORTS 3 - HD",
+        package: "SHAHID SPORTS",
+        quality: "HD",
+        channel: 3,
+        url: "http://sansat.cc:88/02022506639475/7317926030/1079694"
+    },
+    {
+        name: "SHAHID SPORTS 3 - SD",
+        package: "SHAHID SPORTS",
+        quality: "SD",
+        channel: 3,
+        url: "http://sansat.cc:88/02022506639475/7317926030/1079698"
+    },
+    {
+        name: "SHAHID SPORTS 3 - LOW",
+        package: "SHAHID SPORTS",
+        quality: "LOW",
+        channel: 3,
+        url: "http://sansat.cc:88/02022506639475/7317926030/1079701"
+    },
+
+    {
+        name: "SHAHID SPORTS 4 - 4K",
+        package: "SHAHID SPORTS",
+        quality: "4K",
+        channel: 4,
+        url: "http://sansat.cc:88/02022506639475/7317926030/1079687"
+    },
+    {
+        name: "SHAHID SPORTS 4 - FHD",
+        package: "SHAHID SPORTS",
+        quality: "FHD",
+        channel: 4,
+        url: "http://sansat.cc:88/02022506639475/7317926030/1079691"
+    },
+    {
+        name: "SHAHID SPORTS 4 - HD",
+        package: "SHAHID SPORTS",
+        quality: "HD",
+        channel: 4,
+        url: "http://sansat.cc:88/02022506639475/7317926030/1079695"
+    },
+    {
+        name: "SHAHID SPORTS 4 - SD",
+        package: "SHAHID SPORTS",
+        quality: "SD",
+        channel: 4,
+        url: "http://sansat.cc:88/02022506639475/7317926030/1079699"
+    },
+    {
+        name: "SHAHID SPORTS 4 - LOW",
+        package: "SHAHID SPORTS",
+        quality: "LOW",
+        channel: 4,
+        url: "http://sansat.cc:88/02022506639475/7317926030/1079703"
+    },
+
+
+    // =========================================================
+    // SHASHA - EVENT ONLY
+    // =========================================================
+
+    {
+        name: "SHASHA 1",
+        package: "SHASHA",
+        url: "http://sansat.cc:88/02022506639475/7317926030/1302037"
+    },
+    {
+        name: "SHASHA 2",
+        package: "SHASHA",
+        url: "http://sansat.cc:88/02022506639475/7317926030/1302038"
+    },
+    {
+        name: "SHASHA 3",
+        package: "SHASHA",
+        url: "http://sansat.cc:88/02022506639475/7317926030/1302039"
+    },
+
+
+    // =========================================================
+    // DAZN
+    // =========================================================
+
+    {
+        name: "DAZN F1",
+        package: "DAZN",
+        url: "http://sansat.cc:88/02022506639475/7317926030/1079704"
+    },
+
+    {
+        name: "DAZN ONAIR 1",
+        package: "DAZN",
+        url: "http://sansat.cc:88/02022506639475/7317926030/1079705"
+    },
+    {
+        name: "DAZN ONAIR 2",
+        package: "DAZN",
+        url: "http://sansat.cc:88/02022506639475/7317926030/1079706"
+    },
+    {
+        name: "DAZN ONAIR 3",
+        package: "DAZN",
+        url: "http://sansat.cc:88/02022506639475/7317926030/1079707"
+    },
+    {
+        name: "DAZN ONAIR 4",
+        package: "DAZN",
+        url: "http://sansat.cc:88/02022506639475/7317926030/1079708"
+    },
+    {
+        name: "DAZN ONAIR 5",
+        package: "DAZN",
+        url: "http://sansat.cc:88/02022506639475/7317926030/1079709"
+    },
+    {
+        name: "DAZN ONAIR 6",
+        package: "DAZN",
+        url: "http://sansat.cc:88/02022506639475/7317926030/1079710"
+    },
+    {
+        name: "DAZN ONAIR 7",
+        package: "DAZN",
+        url: "http://sansat.cc:88/02022506639475/7317926030/1079711"
+    },
+    {
+        name: "DAZN ONAIR 8",
+        package: "DAZN",
+        url: "http://sansat.cc:88/02022506639475/7317926030/1079712"
+    },
+    {
+        name: "DAZN ONAIR 9",
+        package: "DAZN",
+        url: "http://sansat.cc:88/02022506639475/7317926030/1079713"
+    },
+    {
+        name: "DAZN ONAIR 10",
+        package: "DAZN",
+        url: "http://sansat.cc:88/02022506639475/7317926030/1079714"
+    },
+    {
+        name: "DAZN ONAIR 11",
+        package: "DAZN",
+        url: "http://sansat.cc:88/02022506639475/7317926030/1079715"
+    },
+
+
+    // =========================================================
     // THAMANYA
-    // ==============================
+    // =========================================================
 
     {
         name: "THAMANYA 1HD",
         package: "THAMANYA",
         url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=1936356&extension=ts"
     },
-
     {
         name: "THAMANYA 2HD",
         package: "THAMANYA",
         url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=1936355&extension=ts"
     },
-
     {
         name: "THAMANYA 3HD",
         package: "THAMANYA",
@@ -182,40 +515,35 @@ const channels = [
     },
 
 
-    // ==============================
+    // =========================================================
     // ALKASS SPORT
-    // ==============================
+    // =========================================================
 
     {
         name: "ALKASS SPORT 1HD",
         package: "ALKASS SPORT",
         url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=591593&extension=ts"
     },
-
     {
         name: "ALKASS SPORT 2HD",
         package: "ALKASS SPORT",
         url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=591591&extension=ts"
     },
-
     {
         name: "ALKASS SPORT 3HD",
         package: "ALKASS SPORT",
         url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=787903&extension=ts"
     },
-
     {
         name: "ALKASS SPORT 4HD",
         package: "ALKASS SPORT",
         url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=591589&extension=ts"
     },
-
     {
         name: "ALKASS SPORT 5HD",
         package: "ALKASS SPORT",
         url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=591587&extension=ts"
     },
-
     {
         name: "ALKASS SPORT 6HD",
         package: "ALKASS SPORT",
@@ -223,16 +551,15 @@ const channels = [
     },
 
 
-    // ==============================
+    // =========================================================
     // AD SPORT
-    // ==============================
+    // =========================================================
 
     {
         name: "AD SPORT 1HD",
         package: "AD SPORT",
         url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=993336&extension=ts"
     },
-
     {
         name: "AD SPORT 2HD",
         package: "AD SPORT",
@@ -240,22 +567,20 @@ const channels = [
     },
 
 
-    // ==============================
+    // =========================================================
     // DUBAI SPORT
-    // ==============================
+    // =========================================================
 
     {
         name: "DUBAI SPORT 1HD",
         package: "DUBAI SPORT",
         url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=8086&extension=ts"
     },
-
     {
         name: "DUBAI SPORT 2HD",
         package: "DUBAI SPORT",
         url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=84251&extension=ts"
     },
-
     {
         name: "DUBAI SPORT 3HD",
         package: "DUBAI SPORT",
@@ -263,9 +588,9 @@ const channels = [
     },
 
 
-    // ==============================
+    // =========================================================
     // IRAQIA SPORT
-    // ==============================
+    // =========================================================
 
     {
         name: "IRAQIA SPORT HD",
