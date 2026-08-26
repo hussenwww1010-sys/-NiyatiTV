65864248a8db84711f90c6ba73e5d73d8a054dff
hussenwww1010-sys <hussenwww1010@gmail.com>
2026-08-26 04:15:49 +0300

    Update channels.js

--- channels.js
+++ channels.js
@@ -1,236 +1,276 @@
 const channels = [
-    // =========================
-    // beIN SPORTS
-    // =========================
+
+    // ==============================
+    // beIN SPORTS - SOURCE 1
+    // ==============================
 
     {
         name: "beIN SPORT 1HD",
         package: "beIN SPORTS",
-        url: ""
+        url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=1330437&extension=ts"
     },
+
     {
         name: "beIN SPORT 2HD",
         package: "beIN SPORTS",
-        url: ""
+        url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=1330438&extension=ts"
     },
+
     {
         name: "beIN SPORT 3HD",
         package: "beIN SPORTS",
-        url: ""
+        url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=1411381&extension=ts"
     },
+
     {
         name: "beIN SPORT 4HD",
         package: "beIN SPORTS",
-        url: ""
+        url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=1411380&extension=ts"
     },
+
     {
         name: "beIN SPORT 5HD",
         package: "beIN SPORTS",
-        url: ""
+        url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=1411379&extension=ts"
     },
+
     {
         name: "beIN SPORT 6HD",
         package: "beIN SPORTS",
-        url: ""
+        url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=1411378&extension=ts"
     },
+
     {
         name: "beIN SPORT 7HD",
         package: "beIN SPORTS",
-        url: ""
+        url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=1411377&extension=ts"
     },
+
     {
         name: "beIN SPORT 8HD",
         package: "beIN SPORTS",
-        url: ""
+        url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=1411376&extension=ts"
     },
+
     {
         name: "beIN SPORT 9HD",
         package: "beIN SPORTS",
-        url: ""
+        url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=1411375&extension=ts"
     },
 
-    // =========================
-    // beIN SPORTS - مصدر بديل
-    // =========================
+
+    // ==============================
+    // beIN SPORTS - SOURCE 2
+    // ==============================
 
     {
         name: "beIN SPORT 1HD - 2",
         package: "beIN SPORTS",
-        url: ""
+        url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=1660413&extension=ts"
     },
+
     {
         name: "beIN SPORT 2HD - 2",
         package: "beIN SPORTS",
-        url: ""
+        url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=1660411&extension=ts"
     },
+
     {
         name: "beIN SPORT 3HD - 2",
         package: "beIN SPORTS",
-        url: ""
+        url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=1660409&extension=ts"
     },
+
     {
         name: "beIN SPORT 4HD - 2",
         package: "beIN SPORTS",
-        url: ""
+        url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=1660407&extension=ts"
     },
+
     {
         name: "beIN SPORT 5HD - 2",
         package: "beIN SPORTS",
-        url: ""
+        url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=1660405&extension=ts"
     },
+
     {
         name: "beIN SPORT 6HD - 2",
         package: "beIN SPORTS",
-        url: ""
+        url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=1660403&extension=ts"
     },
+
     {
         name: "beIN SPORT 7HD - 2",
         package: "beIN SPORTS",
-        url: ""
+        url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=1660401&extension=ts"
     },
+
     {
         name: "beIN SPORT 8HD - 2",
         package: "beIN SPORTS",
-        url: ""
+        url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=1660399&extension=ts"
     },
+
     {
         name: "beIN SPORT 9HD - 2",
         package: "beIN SPORTS",
-        url: ""
+        url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=1660397&extension=ts"
     },
 
-    // =========================
+
+    // ==============================
     // ALWAN SPORT
-    // =========================
+    // ==============================
 
     {
         name: "ALWAN SPORT 1HD",
         package: "ALWAN SPORT",
-        url: ""
+        url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=1859098&extension=ts"
     },
+
     {
         name: "ALWAN SPORT 2HD",
         package: "ALWAN SPORT",
-        url: ""
+        url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=1859097&extension=ts"
     },
+
     {
         name: "ALWAN SPORT 3HD",
         package: "ALWAN SPORT",
-        url: ""
+        url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=1859096&extension=ts"
     },
+
     {
         name: "ALWAN SPORT 4HD",
         package: "ALWAN SPORT",
-        url: ""
+        url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=1859095&extension=ts"
     },
+
     {
         name: "ALWAN SPORT 5HD",
         package: "ALWAN SPORT",
-        url: ""
+        url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=1859094&extension=ts"
     },
+
     {
         name: "ALWAN SPORT 6HD",
         package: "ALWAN SPORT",
-        url: ""
+        url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=1859093&extension=ts"
     },
 
-    // =========================
+
+    // ==============================
     // THAMANYA
-    // =========================
+    // ==============================
 
     {
         name: "THAMANYA 1HD",
         package: "THAMANYA",
-        url: ""
+        url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=1936356&extension=ts"
     },
+
     {
         name: "THAMANYA 2HD",
         package: "THAMANYA",
-        url: ""
+        url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=1936355&extension=ts"
     },
+
     {
         name: "THAMANYA 3HD",
         package: "THAMANYA",
-        url: ""
+        url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=1936354&extension=ts"
     },
 
-    // =========================
+
+    // ==============================
     // ALKASS SPORT
-    // =========================
+    // ==============================
 
     {
         name: "ALKASS SPORT 1HD",
         package: "ALKASS SPORT",
-        url: ""
+        url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=591593&extension=ts"
     },
+
     {
         name: "ALKASS SPORT 2HD",
         package: "ALKASS SPORT",
-        url: ""
+        url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=591591&extension=ts"
     },
+
     {
         name: "ALKASS SPORT 3HD",
         package: "ALKASS SPORT",
-        url: ""
+        url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=787903&extension=ts"
     },
+
     {
         name: "ALKASS SPORT 4HD",
         package: "ALKASS SPORT",
-        url: ""
+        url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=591589&extension=ts"
     },
+
     {
         name: "ALKASS SPORT 5HD",
         package: "ALKASS SPORT",
-        url: ""
+        url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=591587&extension=ts"
     },
+
     {
         name: "ALKASS SPORT 6HD",
         package: "ALKASS SPORT",
-        url: ""
+        url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=787906&extension=ts"
     },
 
-    // =========================
+
+    // ==============================
     // AD SPORT
-    // =========================
+    // ==============================
 
     {
         name: "AD SPORT 1HD",
         package: "AD SPORT",
-        url: ""
+        url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=993336&extension=ts"
     },
+
     {
         name: "AD SPORT 2HD",
         package: "AD SPORT",
-        url: ""
+        url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=993337&extension=ts"
     },
 
-    // =========================
+
+    // ==============================
     // DUBAI SPORT
-    // =========================
+    // ==============================
 
     {
         name: "DUBAI SPORT 1HD",
         package: "DUBAI SPORT",
-        url: ""
+        url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=8086&extension=ts"
     },
+
     {
         name: "DUBAI SPORT 2HD",
         package: "DUBAI SPORT",
-        url: ""
+        url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=84251&extension=ts"
     },
+
     {
         name: "DUBAI SPORT 3HD",
         package: "DUBAI SPORT",
-        url: ""
+        url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=591579&extension=ts"
     },
 
-    // =========================
+
+    // ==============================
     // IRAQIA SPORT
-    // =========================
+    // ==============================
 
     {
         name: "IRAQIA SPORT HD",
         package: "IRAQIA SPORT",
-        url: ""
+        url: "http://103.176.90.24/play/live.php?mac=00:1A:79:00:3A:F8&stream=8116&extension=ts"
     }
+
 ];
